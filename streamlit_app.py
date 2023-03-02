ccaf49006c5218ac800d5b0cef26787e31655e6b
henniesc <72693926+henniesc@users.noreply.github.com>
2023-03-02 13:07:28 +0100

    Update streamlit_app.py
    Edited breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My parents healthy diner')
 
-streamlit.header('Breakfast menu')
-streamlit.text('Egg & avokado')
-streamlit.text('Bread')
-streamlit.text('Pourridge')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
