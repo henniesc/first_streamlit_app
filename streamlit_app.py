de33d0e11a29880627f429d5b7d62aae458992b0
henniesc <72693926+henniesc@users.noreply.github.com>
2023-03-02 13:06:45 +0100

    Update streamlit_app.py
    added header and some breakfast options

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents healthy diner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('Egg & avokado')
+streamlit.text('Bread')
+streamlit.text('Pourridge')
